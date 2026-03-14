790cd33c871ee014ec0d65aabff6e9f53cea760f
J-Kajal <kajaljagtap943@gmail.com>
2026-03-14 15:59:59 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -87,7 +87,7 @@ app.post("/api/test-email-reminder", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
